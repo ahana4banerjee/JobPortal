7a09de74118c5eb7cddf630b133d778912b90797
Ahana4Banerjee <banerjeeahana4@gmail.com>
2025-08-02 23:30:26 +0530

    initial commit

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/user"
-export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job"
-export const APPLICATION_API_END_POINT = "http://localhost:3000/api/v1/application"
-export const COMPANY_API_END_POINT = "http://localhost:3000/api/v1/company"
+export const USER_API_END_POINT = "https://jobportal-p4x3.onrender.com/user"
+export const JOB_API_END_POINT = "https://jobportal-p4x3.onrender.com/job"
+export const APPLICATION_API_END_POINT = "https://jobportal-p4x3.onrender.com/application"
+export const COMPANY_API_END_POINT = "https://jobportal-p4x3.onrender.com/company"
